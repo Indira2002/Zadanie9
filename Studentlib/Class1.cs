2c8a376987671dd4cffc3411a44288b037326011
indira <indira.sultanova.2002@mail.ru>
2020-05-24 00:57:26 +0500

    commit zadanie9

---
+++ Studentlib/Class1.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+namespace Studentlib
+{
+    public class Student
+    {
+        public string id;
+        public string fio;
+        public string group;
+        public string data_r;
+        public string vozrast;
+        static List<Student> List = new List<Student>();
+
+        static void Add(string id, string fio, string data_r, string group,string vozrast)
+        {
+            List.Add(new Student() { id = id, fio = fio, data_r = data_r, group = group, vozrast = vozrast });
+        }
+
+
+        static void Del(string id)
+        {
+            for (int i = 0; i < List.Count; i++)
+            {
+                if (List[i].id == id) List.RemoveAt(i);
+            }
+        }
+
+        static void Change(string id, string fio, string data_r, string group, string vozrast)
+        {
+
+            for (int i = 0; i < List.Count; i++)
+            {
+                if (List[i].id == id)
+                {
+                    List[i].fio = fio;
+                    List[i].data_r = data_r;
+                    List[i].group = group;
+                    List[i].vozrast = vozrast;
+                }
+            }
+
+        }
+
+        static void ShowFio()
+        {
+            for (int i = 0; i < List.Count; i++)
+            {
+                Console.WriteLine((i + 1) + " " + List[i].fio);
+            }
+            Console.WriteLine();
+        }
+
+        static void ShowId(string id)
+        {
+            for (int i = 0; i < List.Count; i++)
+            {
+                if (List[i].id == id)
+                {
+                    Console.WriteLine(List[i].id);
+                    Console.WriteLine(List[i].fio);
+                    Console.WriteLine(List[i].data_r);
+                    Console.WriteLine(List[i].group);
+                    Console.WriteLine(List[i].vozrast);
+                }
+
+            }
+            Console.WriteLine();
+        }
+        static void ShowData_r(string id)
+        {
+            for (int i = 0; i < List.Count; i++)
+            {
+                if (List[i].id == id)
+                {
+                    Console.WriteLine(List[i].data_r);
+                }
+
+            }
+            Console.WriteLine();
+        }
+        static void ShowA(string vozrast)
+        {
+            for (int i = 0; i < List.Count; i++)
+            {
+                if((List[i].vozrast == "18")||(List[i].vozrast == "19") || (List[i].vozrast == "18") || (List[i].vozrast == "20") || (List[i].vozrast == "21"))
+                {
+                    Console.WriteLine("a");
+                }
+            }
+        }
+        static void ShowS(string vozrast)
+        {
+            for (int i = 0; i < List.Count; i++)
+            {
+                if ((List[i].vozrast == "17")|| (List[i].vozrast == "16") || (List[i].vozrast == "15") || (List[i].vozrast == "14"))
+                {
+                    Console.WriteLine("s");
+                }
+            }
+        }
+        public void Phoisk()
+        {
+            Console.WriteLine("Введите фио:");
+            string i = Console.ReadLine();
+            bool ft = false;
+            foreach (var student in List)
+            {
+                Student st = (Student)student;
+                if (i == st.fio)
+                {
+                    List.Add(st);
+                    ft = true;
+                }
+            }
+            if (!ft)
+            {
+                Console.WriteLine("Ошибка!");
+                return;
+
+            }
+            foreach (var student in List)
+            {
+                Console.WriteLine(student.ToString());
+            }
+        }
+    }
+}
---
+++ zadanie9/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Studentlib;
+namespace zadanie9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student st = new Student();
+            int s;
+            s = Convert.ToInt32(Console.ReadLine());
+            switch (s)
+            {
+                case 1:
+                    st.Add();
+                    break;
+                case 2:
+                    st.Del();
+                    break;
+                case 3:
+                    st.Change();
+                    break;
+                case 4:
+                    st.ShowFio();
+                    break;
+                case 5:
+                    st.ShowId();
+                    break;
+                case 6:
+                    st.ShowData_r();
+                    break;
+                case 7:
+                    st.ShowA();
+                    break;
+                case 8:
+                    st.ShowS();
+                    break;
+                case 9:
+                    st.Phoisk();
+                    break;
+                case 10:
+                    Environment.Exit(0);
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
